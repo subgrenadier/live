e1e4cbafc721f9985d9b7b086a494ec5ec9d1876
Aizat <subgrenadier@gmail.com>
2024-10-22 01:21:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player';
+import { Modal, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
+  const [showAd, setShowAd] = useState(true);
+
+  // Handle closing the popup and opening the affiliate link
+  const handleCloseAd = () => {
+    window.open('https://shopee.com.my', '_blank');
+    setShowAd(false);
+  };
+
+  useEffect(() => {
+    // Display ad popup when user visits the site
+    const timer = setTimeout(() => setShowAd(true), 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center my-4">Live Stream</h1>
+
+      {/* Video Player */}
+      <div className="d-flex justify-content-center">
+        <ReactPlayer
+          url="http://206.189.80.101:8000/live/stream1/index.m3u8"
+          playing
+          controls
+          width="80%"
+          height="500px"
+        />
+      </div>
+
+      {/* Ad Popup */}
+      <Modal show={showAd} onHide={handleCloseAd} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Special Offer</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Check out this amazing deal! Click below to explore.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleCloseAd}>
+            Close Ad
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
