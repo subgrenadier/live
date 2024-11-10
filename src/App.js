11451c8bb40542e03d27b0b8eb8bf254b0ca0937
Aizat <subgrenadier@gmail.com>
2024-11-10 18:35:51 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -13,10 +13,7 @@ const App = () => {
   const [videoHeight, setVideoHeight] = useState('500px'); // Default height for video
 
   const adLinks = useMemo(() => [
-    'https://s.shopee.com.my/7fIQKBBX4q',
-    'https://s.shopee.com.my/3fmHYlsho0',
-    'https://s.shopee.com.my/3q5hlBkqN5',
-    'https://s.shopee.com.my/5pqm8vbXsB'
+
   ], []);
 
   useEffect(() => {
@@ -53,24 +50,24 @@ const App = () => {
     <>
       {/* SEO Meta Tags */}
       <Helmet>
-        <title>Live Stream: Jeonbuk FC vs Selangor FC | Berita Viral</title>
-        <meta name="description" content="Watch Jeonbuk FC vs Selangor FC live streaming. Stay updated with match info and exciting offers." />
-        <meta property="og:title" content="Live Stream: Jeonbuk FC vs Selangor FC" />
+        <title>Live Stream: Perak FC vs JDT FC | Berita Viral</title>
+        <meta name="description" content="Watch Perak FC vs JDT FC live streaming. Stay updated with match info and exciting offers." />
+        <meta property="og:title" content="Live Stream: Perak FC vs JDT FC" />
         <meta property="og:description" content="Join the live stream and enjoy the exciting match!" />
-        <link rel="canonical" href="https://selangorlive.netlify.app/" />
-        <meta property="og:image" content="https://i.ytimg.com/vi/KqTJM_abAiY/maxresdefault.jpg" />
+        <link rel="canonical" href="https://jdtlive.netlify.app/" />
+        <meta property="og:image" content="https://i.ytimg.com/vi/3zsSrH12xrM/hq720.jpg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
       </Helmet>
 
       <Container fluid className="dark-mode">
-        <h2 className="text-center">Live Stream: Jeonbuk FC vs Selangor FC</h2>
+        <h2 className="text-center">Live Stream: Perak FC vs JDT FC</h2>
 
 
         {/* Video Player */}
         <Row className="justify-content-center mb-4">
           <Col xs={12} md={8}>
-            <VideoPlayer height={videoHeight} url="https://berita-viral.com/live/selangor/index.m3u8" />
+            <VideoPlayer height={videoHeight} url="https://berita-viral.com/live/JDT/index.m3u8" />
           </Col>
         </Row>
 
@@ -78,14 +75,14 @@ const App = () => {
 
           {/* Add the Social Share Component */}
           <h4 className="text-center my-3">Share this match:</h4>
-          <SocialShare title="Live Stream: Jeonbuk FC vs Selangor FC" url="https://selangorlive.netlify.app/" />
+          <SocialShare title="Live Stream: Perak FC vs JDT FC" url="https://jdtlive.netlify.app/" />
 
           <div className="info-container" style={{ marginTop: '10em' }}>
             <h5>Info Siaran Langsung & Live Streaming:</h5>
             <p>
-              <strong>Tarikh:</strong> 7 November 2024 (Khamis)<br />
-              <strong>Masa:</strong> 6.00 pm<br />
-              <strong>Venue:</strong>Jeonju World Cup Stadium
+              <strong>Tarikh:</strong> 10 November 2024 (Ahad)<br />
+              <strong>Masa:</strong> 8.15 pm<br />
+              <strong>Venue:</strong>Stadium Sultan Ibrahim, Iskandar Puteri, Johor
             </p>
           </div>
 
