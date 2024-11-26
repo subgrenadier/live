f4f8efdb350111c908c3c18ae91d0bdd31da80da
Aizat <subgrenadier@gmail.com>
2024-11-26 19:47:18 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -13,8 +13,11 @@ const App = () => {
   const [videoHeight, setVideoHeight] = useState('500px'); // Default height for video
 
   const adLinks = useMemo(() => [
-    'https://s.shopee.com.my/8AF1dm18XT',
-    'https://s.shopee.com.my/9zgfp7mF87'
+    'https://s.shopee.com.my/g9APZacVV',
+    'https://s.shopee.com.my/1B5R0UYiUc',
+    'https://s.shopee.com.my/10m0oBZLpb',
+    'https://s.shopee.com.my/BCtoecWWS',
+    'https://s.shopee.com.my/tTcLd9rR'
   ], []);
 
   useEffect(() => {
@@ -51,24 +54,24 @@ const App = () => {
     <>
       {/* SEO Meta Tags */}
       <Helmet>
-        <title>Live Stream: KL Rovers FC vs JDT FC | Berita Viral</title>
-        <meta name="description" content="Watch KL Rovers FC vs JDT FC live streaming. Stay updated with match info and exciting offers." />
-        <meta property="og:title" content="Live Stream: KL Rovers FC vs JDT FC" />
+        <title>Live Stream: Shandong Taishan FC vs JDT FC | Berita Viral</title>
+        <meta name="description" content="Watch Shandong Taishan FC vs JDT FC live streaming. Stay updated with match info and exciting offers." />
+        <meta property="og:title" content="Live Stream: Shandong Taishan FC vs JDT FC" />
         <meta property="og:description" content="Join the live stream and enjoy the exciting match!" />
         <link rel="canonical" href="https://jdtlive.netlify.app/" />
-        <meta property="og:image" content="https://i.ytimg.com/vi/4gEFmG3r1rg/hq720.jpg" />
+        <meta property="og:image" content="https://i.ytimg.com/vi/4fsS8jShYJI/hq720.jpg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
       </Helmet>
 
       <Container fluid className="dark-mode">
-        <h2 className="text-center">Live Stream: KL Rovers FC vs JDT FC</h2>
+        <h2 className="text-center">Live Stream: Shandong Taishan FC vs JDT FC</h2>
 
 
         {/* Video Player */}
         <Row className="justify-content-center mb-4">
           <Col xs={12} md={8}>
-            <VideoPlayer height={videoHeight} url="https://berita-viral.com/live/JDT/index.m3u8" />
+            <VideoPlayer height={videoHeight} url="https://berita-viral.com/live/taishan/index.m3u8" />
           </Col>
         </Row>
 
@@ -76,14 +79,14 @@ const App = () => {
 
           {/* Add the Social Share Component */}
           <h4 className="text-center my-3">Share this match:</h4>
-          <SocialShare title="Live Stream: KL Rovers FC vs JDT FC" url="https://jdtlive.netlify.app/" />
+          <SocialShare title="Live Stream: Shandong Taishan FC vs JDT FC" url="https://jdtlive.netlify.app/" />
 
           <div className="info-container" style={{ marginTop: '10em' }}>
             <h5>Info Siaran Langsung & Live Streaming:</h5>
             <p>
-              <strong>Tarikh:</strong> 20 November 2024 (Rabu)<br />
-              <strong>Masa:</strong> 6.00 pm<br />
-              <strong>Venue:</strong> Stadium Sultan Ibrahim, Iskandar Puteri, Johor
+              <strong>Tarikh:</strong> 26 November 2024 (Selasa)<br />
+              <strong>Masa:</strong> 8.00 pm<br />
+              <strong>Venue:</strong> Jinan Olympics Sports Center
             </p>
           </div>
 
@@ -92,11 +95,11 @@ const App = () => {
         {/* Ad Popup */}
         <Modal show={showAd} onHide={handleCloseAd} centered dialogClassName="custom-modal">
           <Modal.Header closeButton>
-            <Modal.Title>Shopee 25.11 Big Sale</Modal.Title>
+            <Modal.Title>Shopee 12.12 Birthday Sale</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <p>Shopee Live 70% OFF. Lowest Price Guaranteed. Free Shipping. No Min Spend. Check out this amazing deal! Click below to explore.</p>
-            <Image src={`${process.env.PUBLIC_URL}/shopee2511.png`} fluid onClick={handleCloseAd} alt="Shopee Ad"
+            <p>Shopee Live 90% OFF. Lowest Price Guaranteed. Free Shipping. No Min Spend. Check out this amazing deal! Click below to explore.</p>
+            <Image src={`${process.env.PUBLIC_URL}/shopee1212.jpeg`} fluid onClick={handleCloseAd} alt="Shopee Ad"
               style={{ width: '100%' }} />
           </Modal.Body>
           <Modal.Footer>
